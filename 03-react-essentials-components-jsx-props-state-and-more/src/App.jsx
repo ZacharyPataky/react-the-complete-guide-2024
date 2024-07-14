bd9b4fc31ccebc9a0cd0c2727808fac226703d1b
Zachary Pataky <zacharypataky@gmail.com>
2024-07-14 18:28:45 -0400

    PROGRES: Passing custom arguments to event functions

--- 03-react-essentials-components-jsx-props-state-and-more/src/App.jsx
+++ 03-react-essentials-components-jsx-props-state-and-more/src/App.jsx
@@ -4,8 +4,9 @@ import TabButton from "./components/TabButton/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
-  function handleClick() {
-    console.log("Hello World - Clicked!");
+  function handleClick(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
   }
 
   return (
@@ -28,10 +29,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
