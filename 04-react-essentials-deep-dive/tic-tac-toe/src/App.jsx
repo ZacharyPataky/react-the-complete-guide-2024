8a9e81839db55a4864aaf3ea89fe92235ff0d9fa
Zachary A. Pataky <ZPataky@MerchantsGroup.com>
2024-07-18 12:47:32 -0400

    PROGRESS: Lifting computed values up.

--- 04-react-essentials-deep-dive/tic-tac-toe/src/App.jsx
+++ 04-react-essentials-deep-dive/tic-tac-toe/src/App.jsx
@@ -4,6 +4,12 @@ import { useState } from "react";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
 function deriveActivePlayer(gameTurns) {
   return gameTurns.length > 0 && gameTurns[0].player === "X" ? "O" : "X";
 }
@@ -12,6 +18,21 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[];
+    const secondSquareSymbol;
+    const thirdSquareSymbol;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -41,7 +62,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} gameBoard={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
