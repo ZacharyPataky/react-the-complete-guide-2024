f25c787a7d46d40a1e04c3cba91a3f2f29987090
Zachary A. Pataky <ZPataky@MerchantsGroup.com>
2024-07-18 10:20:55 -0400

    PROGRESS: Best Practice: Updating object state immutably

--- 04-react-essentials-deep-dive/tic-tac-toe/src/components/GameBoard.jsx
+++ 04-react-essentials-deep-dive/tic-tac-toe/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleClickSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // We're inherently inheriting data; we want the previous state
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ]; // Paste in all old elements
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleClickSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
