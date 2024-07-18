1d36808141b9971feca5caaddc33cc4ea95acaab
Zachary A. Pataky <ZPataky@MerchantsGroup.com>
2024-07-18 12:34:15 -0400

    PROGRESS: Outsourcing data into a separate file.

--- 04-react-essentials-deep-dive/tic-tac-toe/src/App.jsx
+++ 04-react-essentials-deep-dive/tic-tac-toe/src/App.jsx
@@ -2,6 +2,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 
 function deriveActivePlayer(gameTurns) {
   return gameTurns.length > 0 && gameTurns[0].player === "X" ? "O" : "X";
