3bf72ac7cb2bdcc989adb3af479e891e142d1b4e
Zachary A. Pataky <ZPataky@MerchantsGroup.com>
2024-07-18 14:31:16 -0400

    PROGRESS: An alternative to lifting state up.

--- 04-react-essentials-deep-dive/tic-tac-toe/src/App.jsx
+++ 04-react-essentials-deep-dive/tic-tac-toe/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -88,11 +88,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
