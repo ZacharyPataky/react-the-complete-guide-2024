a28dcea7b9972472c029e80001c03738f802ca37
Zachary A. Pataky <ZPataky@MerchantsGroup.com>
2024-07-22 12:16:20 -0400

    PROGRESS: Computing values and properly handling number values.

--- 05-react-essentials-practice-project/src/components/Results.jsx
+++ 05-react-essentials-practice-project/src/components/Results.jsx
@@ -1,3 +1,6 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+
 export default function Results({ userInput }) {
+  const resultsData = calculateInvestmentResults(userInput);
   return <p>Results</p>;
 }
